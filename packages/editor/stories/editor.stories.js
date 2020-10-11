15450dfa155c30843eb3677a3057eeceac4444b3
udaypydi <udaypydi333@gmail.com>
2020-10-11 18:37:10 +0530

    fix(storybooks): stories not being loaded

--- packages/editor/stories/editor.stories.js
+++ packages/editor/stories/editor.stories.js
@@ -5,3 +5,12 @@ export default {
     title: 'Editor',
     component: App
 }
+
+const Template = (args) => <App {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+  label: 'Editor',
+};
