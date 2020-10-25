4733b988588a48c351f6e3b58e06c9b27f71028c
udaypydi <uday@moengage.com>
2020-10-25 23:38:47 +0530

    feat(sidebar): move inline styles to scss

--- packages/editor/src/components/sidebar/sidebar.jsx
+++ packages/editor/src/components/sidebar/sidebar.jsx
@@ -3,40 +3,17 @@ import { Segment, Icon } from 'semantic-ui-react';
 import sidebarConfig from './sidebar.constant';
 import './sidebar.scss';
 
-const styles = {
-    sidebarContainer: {
-         width: '20%', 
-         backgroundColor: '#ffffff', 
-         border: '1px solid #ccc',
-         display: 'flex',
-         alignItems: 'flex-start',
-         justifyContent: 'space-evenly',
-         flexWrap: 'wrap',
-         flex: 1,
-    },
-
-    iconContainer: {
-        height: 100,
-        width: 100,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 0,
-        cursor: 'pointer'
-    }
-};
 
 function Sidebar(props) {
     return(
         <div className="sidebar">
             <Segment 
                 className="container"
-                style={styles.sidebarContainer}
                 raised
             >
                 {
                     sidebarConfig.map(config => (
-                        <Segment style={styles.iconContainer}>
+                        <Segment className="icon">
                             <Icon name={config.icon} style={{ fontSize: 30 }} />
                         </Segment>
                     ))
